35a5d433695f23ec5e9e5f1e1667d75eab90556c
Laion Pereira <kingslaion@gmail.com>
2023-01-24 16:12:09 -0300

    exercicio inputs controlados

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -22,15 +22,28 @@ const onChangeEmail = (event) => {
 }
 
 const sendData = () => {
-  //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
+if (
+  age >= 18 &&
+  name.length > 10 &&
+  name.length < 30 &&
+  email.includes('@')
+
+)
   setFormFlow(2)
-}
+} 
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       {formFlow === 1 ? <NameForm
-      // insira aqui suas props
-      /> : <ConfirmationForm />}
+        name = {name}
+        age = {age}
+        email = {email}
+        onChangeName = {onChangeName}
+        onChangeAge = {onChangeAge}
+        onChangeEmail = {onChangeEmail}
+        sendData = {sendData}/>
+      
+       : <ConfirmationForm />}
     </MainContainer>
   )
 }
